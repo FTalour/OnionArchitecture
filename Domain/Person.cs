1770388a76f1c9cab12d4267152da0ef9429bc41
FTalour <florian.talour@hotmail.fr>
2024-04-28 15:37:24 +0200

    Ajout Domain

---
+++ Domain/Person.cs
@@ -0,0 +1,32 @@
+﻿namespace Domain
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateTime ModifiedDate { get; set; }
+
+        public int Age { get; set; }
+
+        public void UpdateAge()
+        {
+            Age++;
+        }
+
+        public override string ToString()
+        {
+            string fullNameWithTitle;
+            if (!string.IsNullOrWhiteSpace(Title))
+            {
+                fullNameWithTitle = $"{Title} {FirstName} {LastName}";
+            }
+            else
+            {
+                fullNameWithTitle = $"{FirstName} {LastName}";
+            }
+            return $"{Id} : {fullNameWithTitle} ({ModifiedDate})";
+        }
+    }
+}
--- simpleProject/Program.cs
+++ simpleProject/Program.cs
@@ -1,37 +1,10 @@
 ﻿using System.Globalization;
 using Infrastructure;
+using Domain;
 
 namespace simpleProjet
 {
-    public class Person
-    {
-        public int Id { get; set; }
-        public string? Title { get; set; }
-        public string? FirstName { get; set; }
-        public string? LastName { get; set; }
-        public DateTime ModifiedDate { get; set; }
-
-        public int Age { get; set; }
-
-        public void UpdateAge()
-        {
-            Age++;
-        }
-
-        public override string ToString()
-        {
-            string fullNameWithTitle;
-            if (!string.IsNullOrWhiteSpace(Title))
-            {
-                fullNameWithTitle = $"{Title} {FirstName} {LastName}";
-            }
-            else
-            {
-                fullNameWithTitle = $"{FirstName} {LastName}";
-            }
-            return $"{Id} : {fullNameWithTitle} ({ModifiedDate})";
-        }
-    }
+    
 
     /// <summary>
     /// Classe qui permet de 
